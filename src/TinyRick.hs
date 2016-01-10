3f07c3f346dea0d9e24e0611012882f15b5d0b2f
Luke Iannini <lukexi@me.com>
2016-01-09 20:06:27 -0800

    Move in bits of cubensis's subhalive editors

--- src/TinyRick.hs
+++ src/TinyRick.hs
@@ -3,6 +3,9 @@ module TinyRick
   ) where
 
 import TinyRick.Render as Exports
+import TinyRick.SubHalive as Exports
+import TinyRick.Editor as Exports
+import TinyRick.FindPackageDBs as Exports
 
 
 import ShaderRick as Exports
